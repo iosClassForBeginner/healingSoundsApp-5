ba825a1f6357a5ec3f1bdeee05c9ffc77e6e2174
WataruMaeda <wm19850716@gmail.com>
2016-07-30 13:31:49 +0900

    recommit

--- healingSoundsApp_5/ViewController.swift
+++ healingSoundsApp_5/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import AVFoundation // 👈オーディオプレイヤー使うとき、インポートしましょう
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    var audioPlayer: AVAudioPlayer!
+    
+    // 👇画面が生成されると、まずこの関数が呼ばれます
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 👇オーディオプレイヤーを初期化します。
+        self.initAudioPlayer()
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    // MARK: - Init
+    // 👇オーディオプレイヤーを初期化します。
+    func initAudioPlayer()
+    {
+        // 👇BGMファイルを指定します
+        let filePath = NSBundle.mainBundle().pathForResource("sound", ofType: "mp3")
+        let audioPath = NSURL(fileURLWithPath: filePath!)
+        
+        // 👇オーディオプレイヤーの再生準備します
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: audioPath)
+            audioPlayer.prepareToPlay()
+        } catch {
+            print("Error")
+        }
+    }
+    
+    // MARK: - Action
+    @IBAction func play(sender: AnyObject)
+    {
+        // 👇BGMを再生します
+        audioPlayer.play()
+    }
+    
+    @IBAction func stop(sender: AnyObject)
+    {
+        // 👇BGMを停止します
+        audioPlayer.stop()
+    }
+    
+    // MARK: - Support
+    // 👇メモリが足りなくなったら呼ばれます
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
